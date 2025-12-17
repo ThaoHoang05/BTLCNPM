c6f27ab1336cd94ecdedb78417e65da830a4cf16
nhidh235802 <duongnhi2932@gmail.com>
2025-12-18 03:14:49 +0700

    Login lỗi hoài cíu tttt

--- models/authModel.js
+++ models/authModel.js
@@ -9,7 +9,18 @@ const AuthModel = {
             JOIN vaitro v ON u.vaitroid = v.vaitroid`;
         const result = await poolDangNhapPhanQuyen.query(query);
         return result.rows;
-    }
+    },
+
+    // Hàm tìm user và lấy tên vai trò (role)
+    findUserForLogin: async (username) => {
+        const query = `
+            SELECT u.tendangnhap, u.matkhauhash, v.tenvaitro as role 
+            FROM nguoidung u 
+            JOIN vaitro v ON u.vaitroid = v.vaitroid 
+            WHERE u.tendangnhap = $1`;
+        const result = await poolDangNhapPhanQuyen.query(query, [username]);
+        return result.rows[0];
+    },
 };
 
 module.exports = AuthModel;
