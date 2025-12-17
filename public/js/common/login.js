07e4001dd62d2c8ad855c84077f70e6bbbdacb5f
nhidh235802 <duongnhi2932@gmail.com>
2025-12-18 03:30:29 +0700

    sua loi login

--- public/js/common/login.js
+++ public/js/common/login.js
@@ -1,4 +1,5 @@
-async function getUser() {
+async function getUser(event) {
+    if (event) event.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
