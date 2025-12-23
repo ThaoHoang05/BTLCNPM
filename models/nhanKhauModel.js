140c3f84dd4dedec963feb049ed43939ec538e3d
nhidh235802 <duongnhi2932@gmail.com>
2025-12-24 02:19:13 +0700

    Sửa list nhân khẩu: hiện đc tạm trú theo đc chủ hộ

--- models/nhanKhauModel.js
+++ models/nhanKhauModel.js
@@ -1,6 +1,7 @@
 const { poolQuanLiHoKhau } = require('../config/db');
 
 const NhanKhauModel = {
+    // Query lấy ds nhân khẩu thường trú và tạm trú
     getNhanKhauList: async () => {
         try {
             const query = `
@@ -11,12 +12,41 @@ const NhanKhauModel = {
                     nk.cccd AS "cccd",
                     nk.trangthai AS "trangThai",
                     CASE 
-                        WHEN nk.trangthai = 'Tạm trú' THEN tt.diaphuong 
-                        ELSE CONCAT(hk.sonha, ' ', hk.duong, ', ', hk.phuong, ', ', hk.quan, ', ', hk.tinh)
+                        WHEN nk.trangthai IN ('Tạm trú') THEN 
+                            COALESCE(
+                                CONCAT_WS(', ', 
+                                    NULLIF(hk_host.sonha, ''), 
+                                    NULLIF(hk_host.duong, ''), 
+                                    NULLIF(hk_host.phuong, ''), 
+                                    NULLIF(hk_host.quan, ''), 
+                                    NULLIF(hk_host.tinh, '')
+                                ),
+                                'Địa chỉ chưa xác định'
+                            )
+                        ELSE 
+                            CONCAT_WS(', ', 
+                                NULLIF(hk_goc.sonha, ''), 
+                                NULLIF(hk_goc.duong, ''), 
+                                NULLIF(hk_goc.phuong, ''), 
+                                NULLIF(hk_goc.quan, ''), 
+                                NULLIF(hk_goc.tinh, '')
+                            )
                     END AS "diaChi"
                 FROM nhankhau nk
-                LEFT JOIN hokhau hk ON nk.sohokhau = hk.sohokhau
-                LEFT JOIN tamtru tt ON nk.id = tt.nhankhau_id AND tt.denngay >= CURRENT_DATE
+                LEFT JOIN hokhau hk_goc ON nk.sohokhau = hk_goc.sohokhau
+                
+                LEFT JOIN LATERAL (
+                    SELECT * FROM tamtru 
+                    WHERE nhankhau_id = nk.id 
+                    AND trangthai = 'Còn hạn'
+                    ORDER BY denngay DESC 
+                    LIMIT 1
+                ) tt_active ON true
+
+                LEFT JOIN nhankhau host ON (tt_active.chuho_id = host.id OR (tt_active.chuho_id IS NULL AND tt_active.chuhocccd = host.cccd))
+                LEFT JOIN hokhau hk_host ON host.sohokhau = hk_host.sohokhau
+
+                WHERE nk.trangthai IN ('Thường trú', 'Tạm trú')
                 ORDER BY nk.id ASC
             `;
             
